6f9cbd305d90624dac3d5372dc22a9f27f8bb88a
Kelvin Baiden <denlinato@gmail.com>
2021-10-14 14:32:35 +0000

    cart styling

--- s/cart.js
+++ s/cart.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {Text ,View , StyleSheet , Image ,FlatList} from "react-native"
+import {Text ,View , StyleSheet , Image ,TouchableOpacity, TouchableOpacityBase} from "react-native"
 import {Ionicons , FontAwesome , AntDesign ,Feather }  from "@expo/vector-icons"
 import Navbar from "./navbar"
 import CartItems from "./CartItems"
@@ -51,14 +51,38 @@ export default function Cart({navigation}){
 <Text style={flexx.fontsize}>
     Cart Items
 </Text>
-<Text>
+<Text style={flexx.fontcolor}>
     (3 Items)
 </Text>
     </View>    
     </View>
     <View style={flexx.container}>
         {bikes.map(bike=><CartItems items={bike} />)}
-    </View>
+   
+
+  <View style={flexx.feeContainer}>
+ <View style={flexx.fee}>
+<Text style={flexx.fontcolor}>Subtotal</Text>
+<Text style={flexx.fontsize}> <FontAwesome name="dollar" size={10} color="orangered" />3,400.00</Text>
+ </View>   
+ <View style={flexx.fee}  >
+ <Text style={flexx.fontcolor}>Shipping Fee</Text>
+<Text style={flexx.fontsize}> <FontAwesome name="dollar" size={10} color="orangered" />1,00.00</Text>
+
+ </View> 
+
+ <View style={[flexx.fee  ,{}]}>
+ <Text  style={flexx.fontsize}>Total</Text>
+<Text style={flexx.fontsize}> <FontAwesome name="dollar" size={10} color="orangered" />3,500.00</Text>
+ </View>
+  </View>
+
+<TouchableOpacity  style={flexx.checkout}>
+    <Text style={flexx.checkoutText}>
+        Proceed to Checkout
+    </Text>
+</TouchableOpacity>
+  </View>
  <Navbar navigate={navigation.navigate}/>
         </View>
     )
@@ -69,16 +93,31 @@ const flexx=  StyleSheet.create({
     
     container:{
         width:"100%",
-        height:"90%"
+        height:"85%"
         ,
         padding:5,
         marginTop:15
     },
     fontsize:{
-        fontSize:20
+        fontSize:20, fontWeight:"500"
     }
     ,
     fontcolor:{
-color:"#6a6c6d"
+color:"#6a6c6d", fontSize:16
+    },
+    fee:{
+        flexDirection:"row" , justifyContent:"space-between" , alignItems:"center",
+         padding:6
+    },
+    feeContainer:{
+        backgroundColor:"#eee" , width:"95%" , margin:10 , borderRadius:10 ,
+        marginTop:30 , padding:10
+    }
+    ,
+    checkout:{
+        backgroundColor:"orangered" , borderRadius:10,margin:30, marginTop:25   , padding:20
+    },
+    checkoutText:{
+        fontSize:20 , fontWeight:"400",color:"#fff", textAlign:"center"
     }
 })
